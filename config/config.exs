8fdfe4ca010176cd72dc129e90a92ce1a258b41c
Luke Clancy <lukeclancy@hotmail.com>
2024-08-20 12:23:08 -0700

    pushing main to server

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,7 @@
 import Config
 
 parse_env_vars = fn (filename) ->
+  Logger.warning("FILENAME #{filename}")
   {:ok, txt} = File.read(filename)
   Logger.warning("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
   System.put_env(
@@ -46,9 +47,11 @@ env_files = %{
 }
 case config_env() do
   :prod ->
+    Logger.warning("SETTING UP PRODUCTION")
     parse_env_vars.(Map.fetch!(env_files, :prod))
     parse_env_vars.(Map.fetch!(env_files, :shared_prod))
     _ ->
+    Logger.warning("SETTING UP DEVELOPMENT")
     parse_env_vars.(Map.fetch!(env_files, :dev))
     parse_env_vars.(Map.fetch!(env_files, :shared))
 end
